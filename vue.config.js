daea1b1e99c852b794410647c64f25059b47c016
Alejandro Millan <darkslave@live.com>
2021-09-13 10:02:31 -0500

    update max assets file

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+  chainWebpack: config => {
+    config.performance.maxEntrypointSize(400000).maxAssetSize(400000)
+  },
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
-      rtlSupport: false
-    }
+      rtlSupport: false,
+    },
   },
-  transpileDependencies: [
-    'quasar'
-  ]
+  transpileDependencies: ['quasar'],
 }
